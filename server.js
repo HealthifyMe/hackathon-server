51285198bce78d7216d3bee953ecc14e2236f7dc
Anil Nayak <anil.nayak@healthifyme.com>
2023-02-10 07:48:03 +0530

    changes

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const express = require('express');
 const app = express();
 // 3. create express route
 app.get('/healthcheck', (req, res) => {
+    console.log('came here')
     res.send('OK');
 })
 // 4. listen on port 3000
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(4141, () => {
+    console.log('Server is listening on port 4141');
 })
